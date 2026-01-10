7d54bf48269d2b6a7ef665686539ea6d0ffc6d1d
Alec Garza <alecmgarza@gmail.com>
2026-01-10 17:57:34 -0600

    Add a quit option and prompts based on user_action

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
+list_open = True
 todo_list = []
 
-user_action = input('Enter a command for what you would like to do: '
-'(A)dd Task, (D)elete Task, or (C)omplete Task. ')
+while list_open:
+    def prompt_user():
+        user_action = input('Enter a command for what you would like to do: '
+        '(A)dd Task, (D)elete Task, (C)omplete Task, or (Q)uit. ')
 
-if user_action == 'A':
-    todo_list.append(input("Enter your task: "))
-    print(todo_list)
+        return user_action
+
+    user_action = prompt_user()
+
+    if user_action == 'A':
+        todo_list.append(input('Enter your task: '))
+        print(todo_list)
+    elif user_action == 'D':
+        delete_index = int(input('Enter the number of the task you would like to delete: ')) - 1
+    elif user_action == 'C': 
+        complete_index = int(input('Enter the number of the task you would like to mark completed: ')) - 1
+    elif user_action == 'Q':
+        break
+    else:
+        print('Invalid command entered.') 
