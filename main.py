58382c3ab54b885de7d386d3f5170cf5c270b82e
Alec Garza <alecmgarza@gmail.com>
2026-01-10 18:15:26 -0600

    User can delete tasks and quit program, handles IndexError when user enters invalid task position

--- main.py
+++ main.py
@@ -14,7 +14,13 @@ while list_open:
         todo_list.append(input('Enter your task: '))
         print(todo_list)
     elif user_action == 'D':
-        delete_index = int(input('Enter the number of the task you would like to delete: ')) - 1
+        delete_pos = int(input('Enter the position of the task you would like to delete: '))
+        try: 
+            todo_list.pop(delete_pos - 1)
+            print(todo_list)
+        except IndexError:
+            print('No task at this position.')
+            print(todo_list)
     elif user_action == 'C': 
         complete_index = int(input('Enter the number of the task you would like to mark completed: ')) - 1
     elif user_action == 'Q':
