ca5967ebdc25e554e76a8087b7910393234fb04a
Alec Garza <alecmgarza@gmail.com>
2026-01-10 18:30:47 -0600

    Include function that prints todo_list in a numbered format

--- main.py
+++ main.py
@@ -7,20 +7,24 @@ while list_open:
         '(A)dd Task, (D)elete Task, (C)omplete Task, or (Q)uit. ')
 
         return user_action
+    
+    def output_list():
+        for i in range(len(todo_list)):
+            print(f'{i + 1}. ' + todo_list[i])
 
     user_action = prompt_user()
 
     if user_action == 'A':
         todo_list.append(input('Enter your task: '))
-        print(todo_list)
+        output_list()
     elif user_action == 'D':
         delete_pos = int(input('Enter the position of the task you would like to delete: '))
         try: 
             todo_list.pop(delete_pos - 1)
-            print(todo_list)
+            output_list()
         except IndexError:
             print('No task at this position.')
-            print(todo_list)
+            output_list()
     elif user_action == 'C': 
         complete_index = int(input('Enter the number of the task you would like to mark completed: ')) - 1
     elif user_action == 'Q':
