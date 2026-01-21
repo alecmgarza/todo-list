88c55f2e7c416bfa079451ac4b2e2ec2aebc80a5
Alec Garza <alecmgarza@gmail.com>
2026-01-20 20:31:26 -0600

    Move functions out of while loop, wrap while loop inside main().

--- main.py
+++ main.py
@@ -1,38 +1,41 @@
 list_open = True
 todo_list = []
 
-while list_open:
-    def prompt_user():
-        user_action = input('Enter a command for what you would like to do: '
-        '(A)dd Task, (D)elete Task, (C)omplete Task, or (Q)uit. ')
+def prompt_user():
+    user_action = input('Enter a command for what you would like to do: '
+    '(A)dd Task, (D)elete Task, (C)omplete Task, or (Q)uit. ')
 
-        return user_action
-    
-    def output_list():
-        for i in range(len(todo_list)):
-            print(f'{i + 1}. ' + todo_list[i])
-    
-    def mark_complete(i):
-        todo_list[i] = '\u0336' + '\u0336'.join(todo_list[i])
+    return user_action
 
-    user_action = prompt_user()
+def output_list():
+    for i in range(len(todo_list)):
+        print(f'{i + 1}. ' + todo_list[i])
 
-    if user_action == 'A':
-        todo_list.append(input('Enter your task: '))
-        output_list()
-    elif user_action == 'D':
-        delete_pos = int(input('Enter the position of the task you would like to delete: '))
-        try: 
-            todo_list.pop(delete_pos - 1)
+def mark_complete(i):
+    todo_list[i] = '\u0336' + '\u0336'.join(todo_list[i])
+
+def main():
+    while list_open:
+        user_action = prompt_user()
+
+        if user_action == 'A':
+            todo_list.append(input('Enter your task: '))
             output_list()
-        except IndexError:
-            print('No task at this position.')
+        elif user_action == 'D':
+            delete_pos = int(input('Enter the position of the task you would like to delete: '))
+            try: 
+                todo_list.pop(delete_pos - 1)
+                output_list()
+            except IndexError:
+                print('No task at this position.')
+                output_list()
+        elif user_action == 'C': 
+            complete_pos = int(input('Enter the position of the task you would like to mark completed: '))
+            mark_complete(complete_pos - 1)
             output_list()
-    elif user_action == 'C': 
-        complete_pos = int(input('Enter the position of the task you would like to mark completed: '))
-        mark_complete(complete_pos - 1)
-        output_list()
-    elif user_action == 'Q':
-        break
-    else:
-        print('Invalid command entered.') 
+        elif user_action == 'Q':
+            break
+        else:
+            print('Invalid command entered.')
+
+main()
